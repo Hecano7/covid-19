0d875c6b9c39522c4ef59a89a7be4cecd228c7ba
Hecan7 <7hecano@gmail.com>
2020-04-21 17:09:42 -0700

    trying filter list depending on item clicked

--- Public/scripts.js
+++ Public/scripts.js
@@ -29,10 +29,13 @@ document.addEventListener("DOMContentLoaded", function () {
         return o;
       }))
       .then(data => data.sort(function(a, b){return a.Dist-b.Dist}))
-      .then((objects) => objects.filter(obj => obj.Name == localStorage.getItem("item")))
-      .then(data => data.map(obj => createRow(obj)))
-      // .then(data => data.map(obj => console.log(obj)))
-      
+      .then(function (objects){if (localStorage.getItem("item") != "all"){
+        var filtered = objects.filter(obj => obj.Name == localStorage.getItem("item"));
+        filtered.map(obj => createRow(obj));
+      }else{
+        objects.map(obj => createRow(obj))
+      }})
+
       )
     })
   }
